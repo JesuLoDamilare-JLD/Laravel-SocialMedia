9e6005d9fb83b2433af2b0021f3d9d7023f0fee7
Dare Emmanuel <dareolumuyiwa@jldaveoplus.com>
2024-02-06 00:05:05 +0000

    progress

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,9 +3,19 @@
 @section('content')
 <div class="container">
    <div class="row">
-    <div class="col-3">
-        
+    <div class="col-3 p-5 d-flex" >
+<img src="/images/dev.jpg" class="rounded-circle" style="height:150px;">
+    <div class="col-9">
+<div class="ps-5">
+    <h1>JLD MeetsUp</h1>
+</div>
+<div class="ps-5">
+    <div><strong>153</strong> posts</div>
+    <div><strong>23k</strong> followers</div>
+    <div><strong>212</strong> following</div>
+</div>
     </div>
+   </div>
    </div>
 
 </div>
