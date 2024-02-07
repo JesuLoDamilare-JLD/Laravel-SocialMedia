28f061f62d89af4ec5587543d0e7d91b5a1791c4
Dare Emmanuel <dareolumuyiwa@jldaveoplus.com>
2024-02-07 22:29:07 +0000

    progress

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,21 +2,38 @@
 
 @section('content')
 <div class="container">
-   <div class="row">
-    <div class="col-3 p-5 d-flex" >
-<img src="/images/dev.jpg" class="rounded-circle" style="height:150px;">
-    <div class="col-9">
-<div class="ps-5">
-    <h1>JLD MeetsUp</h1>
-</div>
-<div class="ps-5">
-    <div><strong>153</strong> posts</div>
-    <div><strong>23k</strong> followers</div>
-    <div><strong>212</strong> following</div>
-</div>
+    <div class="row">
+        <div class="col-3 p-5">
+            <img src="/images/dev.jpg" class="rounded-circle" style="height:150px;">
+        </div>
+        <div class="col-9 pt-5">
+            <div>
+                <h1>JLD MeetsUp</h1>
+            </div>
+            <div class="d-flex">
+                <div class="pe-5"><strong>23k</strong> followers</div>
+                <div class="pe-5"><strong>212</strong> following</div>
+                <div class="pe-5"><strong>153</strong> posts</div>
+            </div>
+            <div class="pt-2"><strong>jldaveoplus.com</strong></div>
+            <div>
+                Welcome to JLD MeetsUP, a vibrant online community where connections are made and conversations happen. Our platform, accessible at jldaveoplus.com, is designed to bring people together, fostering meaningful interactions and shared experiences. Whether you’re looking to meet new people, share your thoughts, or explore exciting content, JLD MeetsUP is the place to be. Join us today and be part of our growing community!
+            </div>
+            <div><a href="#">www.jldaveoplus.com</a></div>
+        </div>
+        <div class="row pt-5">
+            <div class="col-4">
+                <img src="/images/bnm.jpg" style="height: 300px;">
+            </div>
+            <div class="col-4">
+                <img src="/images/soft.jpg" style="height: 300px;">
+            </div>
+            <div class="col-4">
+                <img src="/images/tech.jpg" style="height: 300px;">
+            </div>
+        </div>
+
     </div>
-   </div>
-   </div>
 
 </div>
 @endsection
