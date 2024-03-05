82e58e69179a0fac1b8d8dbe4c88f801ec3f9c2a
Dare Emmanuel <dareolumuyiwa@jldaveoplus.com>
2024-03-05 23:28:00 +0000

    ok

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -2,9 +2,40 @@
 
 @section('content')
 <div class="container">
+    <form action="" method="POST" enctype="multipart/form-data">
+        <div class="row">
+            <div class="col-8 offset-2">
 
+                <div class="row mb-3">
+                    <label for="caption" class="col-md-4 col-form-label ">Post Caption</label>
+
+                    <input id="caption" type="text" class="form-control @error('caption') is-invalid @enderror" caption="caption" value="{{ old('caption') }}" required autocomplete="caption" autofocus>
+
+                    @error('caption')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                    @enderror
+
+                </div>
+
+                <div class="row">
+                    <label for="image" class="col-md-4 col-form-label ">Post Image</label>
+                    <input type="file" , class="form-control-file" id="image" name="image">
+
+                    @error('image')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                    @enderror
+                </div>
+
+            </div>
+        </div>
 
 </div>
+
+</form>
 
 </div>
 @endsection
