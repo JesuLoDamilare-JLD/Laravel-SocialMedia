5ef45caf345f3b35fb945e7aca75e7e5ccb69fe4
Dare Emmanuel <dareolumuyiwa@jldaveoplus.com>
2024-03-09 11:21:58 +0000

    k

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use League\Flysystem\WebDAV\SabreServerTest;
+use Intervention\Image\Facades\Image;
 
 class PostsController extends Controller
 {
@@ -25,6 +27,10 @@ class PostsController extends Controller
         ]);
 
         $imagePath = request("image")->store("uploads", "public");
+
+        $image = Image::make(public_path("storage/{$imagePath}"))->fit(1200, 1200);
+        $image->save();
+
 
         auth()->user()->posts()->create([
             "caption" => $data["caption"],
