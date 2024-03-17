a76daef46d3032b13a97239dcad45192cc1769aa
Dare Emmanuel <dareolumuyiwa@jldaveoplus.com>
2024-03-17 23:47:00 +0000

    Update ProfilesController.php

--- app/Http/Controllers/ProfilesController.php
+++ app/Http/Controllers/ProfilesController.php
@@ -18,7 +18,7 @@ class ProfilesController extends Controller
         return view("profiles.edit", compact("user"));
     }
 
-    public function update()
+    public function update(User $user)
     {
         $data = request()->validate([
             "title" => "required",
