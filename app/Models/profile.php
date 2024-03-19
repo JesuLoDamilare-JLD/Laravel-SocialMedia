036829b9eade1f2b72237a0bc0e7eeb8dfd7f510
Dare Emmanuel <dareolumuyiwa@jldaveoplus.com>
2024-03-19 21:54:27 +0000

    ok

--- app/Models/profile.php
+++ app/Models/profile.php
@@ -9,6 +9,12 @@ class profile extends Model
 {
     protected $guarded = [];
 
+    public function profileImage()
+    {
+        $imagePath = ($this->image) ?  $this->image : "profile/YX7Bq6ukh3Shidycty3QcQy1QMdKZQYUWsBEIWB1.png";
+        return "/storage/" . $imagePath;
+    }
+
     public function user()
     {
         return $this->belongsTo(User::class);
