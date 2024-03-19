56b015d7b1450d7187f13def385112fb4499a4e5
Dare Emmanuel <dareolumuyiwa@jldaveoplus.com>
2024-03-19 22:18:05 +0000

    Update ProfilesController.php

--- app/Http/Controllers/ProfilesController.php
+++ app/Http/Controllers/ProfilesController.php
@@ -37,22 +37,17 @@ class ProfilesController extends Controller
 
             $image = Image::read(public_path("storage/{$imagePath}"))->resize(1000, 1000);
             $image->save();
+
+            $imageArray = ["image" => $imagePath];
         }
 
 
-        /* dd(array_merge(
-            $data,
-            [
-                "image" => $imagePath
-
-            ]
-        )); */
         auth()->user()->profile->update(array_merge(
             $data,
-            [
-                "image" => $imagePath
 
-            ]
+            $imageArray ?? []
+
+
         ));
 
         return redirect("/profile/{$user->id}");
